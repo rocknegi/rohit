f36befb83598d31800be5ba6b7857a143c7b1eba
rocknegi <rocknegi53@gmail.com>
2019-01-04 23:04:43 +0530

    Updates

---
+++ precache-manifest.6c67f462e687b43eec89e2f62cc6c4c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9021af65182d071b8eb7",
+    "url": "/rocknegi/static/css/main.6c88b602.chunk.css"
+  },
+  {
+    "revision": "9021af65182d071b8eb7",
+    "url": "/rocknegi/static/js/main.9021af65.chunk.js"
+  },
+  {
+    "revision": "095fe389c3f2ef8c27c8",
+    "url": "/rocknegi/static/js/1.095fe389.chunk.js"
+  },
+  {
+    "revision": "3765b045f5262c622e2f",
+    "url": "/rocknegi/static/js/runtime~main.3765b045.js"
+  },
+  {
+    "revision": "36c991c9ac08ce84f51db2df8b2f3120",
+    "url": "/rocknegi/index.html"
+  }
+];
